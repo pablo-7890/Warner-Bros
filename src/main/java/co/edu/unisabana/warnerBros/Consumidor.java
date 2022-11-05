25d55a061e23e5c8404c239000ab5ae3c38d80fb
Pablo Lugo <pablolura@unisabana.edu.co>
2022-11-05 13:20:07 -0500

    Creación del consumidor

--- src/main/java/co/edu/unisabana/warnerBros/Consumidor.java
+++ src/main/java/co/edu/unisabana/warnerBros/Consumidor.java
@@ -1,6 +1,6 @@
 package co.edu.unisabana.warnerBros;
 
-import co.edu.unisabana.peliculas.controller.Pelicula;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class Consumidor {
 
     @RabbitListener(queues = {"unisabana.peliculas.notificar"})
-    public void receive(@Payload Pelicula pelicula) {
-        log.info("Escuche de la cola unisabana.peliculas.notificar {}", pelicula.getNombrePelicula());
+    public void receive(@Payload String mensaje) {
+        log.info("Escuche de la cola unisabana.peliculas.notificar {}", mensaje);
     }
 }
