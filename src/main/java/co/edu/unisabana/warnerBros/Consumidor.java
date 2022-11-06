8cf55b401b974ec0c11cf583e10f4cad2b7b98b9
Santiago Arrieta <santiagoarve@unisabana.edu.co>
2022-11-05 19:33:30 -0500

    correcion

--- src/main/java/co/edu/unisabana/warnerBros/Consumidor.java
+++ src/main/java/co/edu/unisabana/warnerBros/Consumidor.java
@@ -1,6 +1,7 @@
 package co.edu.unisabana.warnerBros;
 
 
+import co.edu.unisabana.peliculas.controller.Pelicula;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,8 +11,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class Consumidor {
 
-    @RabbitListener(queues = {"unisabana.peliculas.notificar"})
-    public void receive(@Payload String mensaje) {
-        log.info("Escuche de la cola unisabana.peliculas.notificar {}", mensaje);
+    @RabbitListener(queues = {"unisabana.peliculas.warnerbros"})
+    public void receive(@Payload Pelicula mensaje) {
+        log.info("la pelicula fue publicada {}", mensaje.getNombrePelicula());
     }
 }
